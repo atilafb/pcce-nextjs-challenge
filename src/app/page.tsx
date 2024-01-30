52894b0f6aec2d639a046240c4fdd8617c1fadb9
Atila <atilafb77@gmail.com>
2024-01-30 16:37:08 -0300

    feat: add Custom Autocomplete component integrated with React Hook Form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,47 @@
 "use client";
-import { Box } from "@mui/material";
+import React from "react";
+import { Box, Button } from "@mui/material";
+import { useForm } from "react-hook-form";
+import { AsyncAutocomplete } from "./components/AsyncAutocomplete";
 
 export default function Home() {
+  // o name do controller define direitinho o retorno aqui
+  const { handleSubmit, control, watch } = useForm<{
+    carMaker: string;
+    carModel: string;
+  }>();
+
+  const options = [
+    { id: "1", label: "Option 1" },
+    { id: "2", label: "Option 2" },
+  ];
+
   return (
     <Box p={2}>
       <h1>PCCE NEXTJS CHALLENGE</h1>
-      <p>Begin of Form Challenge Project</p>
+      <form
+        onSubmit={handleSubmit((data) => {
+          console.log("Form data submitted", data);
+        })}
+      >
+        <Box>
+          <Box>{watch("carMaker")}</Box>
+          <AsyncAutocomplete
+            control={control}
+            name={"carMaker"}
+            options={options}
+          />
+          <br />
+          <AsyncAutocomplete
+            control={control}
+            name={"carModel"}
+            options={options}
+          />
+        </Box>
+        <Button variant="contained" type="submit">
+          Enviar
+        </Button>
+      </form>
     </Box>
   );
 }
